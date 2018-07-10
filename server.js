6ff40adeb22c6842381d779cf1023e6a26364f6c
siniestrus <41081695+siniestrus@users.noreply.github.com>
2018-07-10 15:03:02 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(function (req, res) {
 });
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.OPENSHIFT_INTERNAL_PORT || process.env.PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || 'localhost');
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || '0.0.0.0');
 
 app.listen(app.get('port'), app.get('ip'), function () {
     console.log("Express server listening on " + app.get('ip') + ":" + app.get('port'));
